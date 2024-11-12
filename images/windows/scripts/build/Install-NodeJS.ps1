905fabd88c9cf7cf0752a61ff77f8096b56c4796
RaviAkshintala <167848931+RaviAkshintala@users.noreply.github.com>
2024-11-12 14:47:47 +0530

    Update Install-NodeJS.ps1

--- images/windows/scripts/build/Install-NodeJS.ps1
+++ images/windows/scripts/build/Install-NodeJS.ps1
@@ -12,7 +12,7 @@ New-Item -Path $prefixPath -Force -ItemType Directory
 New-Item -Path $cachePath -Force -ItemType Directory
 
 
-$defaultVersion = (Get-ToolsetContent).node.default
+
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/tj/n/master/bin/n' -OutFile "$env:USERPROFILE\n"
 & "$env:USERPROFILE\n" $defaultVersion
 
